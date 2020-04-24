ba89a475c8d120d391a1491ddfce15d6912dc186
Michael Eckert <meckert@salesforce.com>
2020-04-24 15:32:47 +0200

    Tests added incl. artefacts for manual testing

--- force-app/main/default/lwc/excelUpload/__tests__/excelUpload.test.js
+++ force-app/main/default/lwc/excelUpload/__tests__/excelUpload.test.js
@@ -2,6 +2,7 @@
 
 import { createElement } from 'lwc';
 import ExcelUpload from 'c/excelUpload';
+import { updateRecord } from 'lightning/uiRecordApi';
 
 jest.mock(
     'lightning/platformResourceLoader',
@@ -18,11 +19,28 @@ jest.mock(
     { virtual: true }
 );
 
-function flushPromises() {
-    // eslint-disable-next-line no-undef
-    return new Promise(resolve => setImmediate(resolve));
+// eslint-disable-next-line no-extend-native
+Promise.prototype.flushPromises = function() {
+    return this.then( () => { return new Promise(resolve => window.setImmediate(resolve)); } );
 }
 
+// eslint-disable-next-line no-extend-native
+Promise.prototype.flushPromisesUntil = function(maxTries, fn) {    
+    return this.then( () => {
+        if(maxTries <= 0) {
+            throw new Error(`Max number of tries in flushPromisesUntil reached`);
+        }
+        else if(fn()) {
+            return new Promise(resolve => { window.setImmediate(resolve) });
+        }
+        else {
+            //return new Promise(resolve => { window.setImmediate(resolve) }).then( () => flushPromisesUntil(maxTries-1, fn) );
+            return (new Promise(resolve => { window.setImmediate(resolve) })).flushPromisesUntil(maxTries-1, fn);
+        }        
+    });
+}
+
+/*
 function flushPromisesUntil(maxTries, fn) {    
     if(maxTries <= 0) {
         throw new Error(`Max number of tries in flushPromisesUntil reached`);
@@ -34,6 +52,7 @@ function flushPromisesUntil(maxTries, fn) {
         return new Promise(resolve => { window.setImmediate(resolve) }).then( () => flushPromisesUntil(maxTries-1, fn) );
     }    
 }
+*/
 
 describe('c-excel-upload', () => {
     const TITLE = "Upload Excel File";
@@ -69,7 +88,8 @@ describe('c-excel-upload', () => {
         );
 
         test('Failure to load XSLX', () => {
-            return flushPromises()
+            return Promise.resolve()
+            .flushPromises()
             .then( () => {
                 const card = element.shadowRoot.querySelector('lightning-card');           
                 // an error message should be displayed
@@ -101,8 +121,8 @@ describe('c-excel-upload', () => {
             input.files = [];
             expect(input).toBeTruthy();            
             input.dispatchEvent(new CustomEvent('change'), { target: input } );   
-            return flushPromises()                     
         })
+        .flushPromises()
         .then( () => {      
             // Card is still there                  
             const card = element.shadowRoot.querySelector('lightning-card');
@@ -121,9 +141,9 @@ describe('c-excel-upload', () => {
             const footerBtn = modal.querySelector('lightning-button[title="Ok"]');
             expect(footerBtn).toBeTruthy();
             // click the button in the upper right (next test clicks the one in the footer)
-            closeBtn.dispatchEvent(new CustomEvent('click'));
-            return flushPromises();
+            closeBtn.dispatchEvent(new CustomEvent('click'));            
         })
+        .flushPromises()
         .then( () => {
             // Card is still there                  
             const card = element.shadowRoot.querySelector('lightning-card');
@@ -141,9 +161,9 @@ describe('c-excel-upload', () => {
             const input = card.querySelector('lightning-input');
             expect(input).toBeTruthy();
             input.files = [ new Blob(), new Blob() ];            
-            input.dispatchEvent(new CustomEvent('change'), { target: input } );   
-            return flushPromises()                     
+            input.dispatchEvent(new CustomEvent('change'), { target: input } );                           
         })
+        .flushPromises()  
         .then( () => {      
             // Card is still there                  
             const card = element.shadowRoot.querySelector('lightning-card');
@@ -162,9 +182,9 @@ describe('c-excel-upload', () => {
             const footerBtn = modal.querySelector('lightning-button[title="Ok"]');
             expect(footerBtn).toBeTruthy();
             // click the button in the footer (previous test clicks the one in the upper right)
-            footerBtn.dispatchEvent(new CustomEvent('click'));
-            return flushPromises();
+            footerBtn.dispatchEvent(new CustomEvent('click'));            
         })
+        .flushPromises()
         .then( () => {
             // Card is still there                  
             const card = element.shadowRoot.querySelector('lightning-card');
@@ -180,27 +200,26 @@ describe('c-excel-upload', () => {
         .then( () => {
             const card = element.shadowRoot.querySelector('lightning-card'); 
             const input = card.querySelector('lightning-input');
-            expect(input).toBeTruthy();
-            
+            expect(input).toBeTruthy();                        
+
             const file = new Blob();
             file.lastModifed = Date.now();
             file.lastModifedDate = new Date();
             file.name = "empty.xlsx";            
             input.files = [ file ];   
-            input.dispatchEvent(new CustomEvent('change'), { target: input } );   
-            return flushPromises()                     
+            input.dispatchEvent(new CustomEvent('change'), { target: input } );             
         })
+        .flushPromises()
         .then( () => {
             const modal = element.shadowRoot.querySelector('.slds-modal');
             expect(modal).toBeTruthy();        
  
-            expect(modal.textContent).toMatch(/Reading File/);
-            return flushPromisesUntil(3, () => {
-                // eslint-disable-next-line no-shadow
-                const modal = element.shadowRoot.querySelector('.slds-modal');                
-                return modal.textContent.match(/Error/);
-            });    
+            expect(modal.textContent).toMatch(/Reading File/);  
         })
+        .flushPromisesUntil(3, () => {            
+            const modal = element.shadowRoot.querySelector('.slds-modal');                
+            return modal.textContent.match(/Error/);
+        })          
         .then( () => {
             const modal = element.shadowRoot.querySelector('.slds-modal');
             expect(modal).toBeTruthy();                        
@@ -214,9 +233,8 @@ describe('c-excel-upload', () => {
     test('Upload good file', () => {
         return Promise.resolve()
         .then( () => {
+            element.recordId = "00X123456789012345678";
             // field1 skipped intentionally, things should work if fields are left blank
-            element.field2 = "field1__c";
-            element.address2 = "A1";
             element.field2 = "field2__c";
             element.address2 = "First!B2";
             element.field3 = "field3__c";
@@ -230,7 +248,8 @@ describe('c-excel-upload', () => {
             element.field9 = "field9__c";
             element.address9 = "C2";
             element.field10 = "field10__c";
-            element.address10 = "C3";
+            element.address10 = "A1";
+
 
             const card = element.shadowRoot.querySelector('lightning-card'); 
             const input = card.querySelector('lightning-input');
@@ -238,14 +257,47 @@ describe('c-excel-upload', () => {
             
             const fs = require('fs');            
             jest.dontMock('fs');
-            const file = fs.readFileSync('./test-data/test.xlsx');            
-            
+            const fileBuffer = fs.readFileSync('./test-data/test.xlsx');  
+            const array = new Uint8Array(fileBuffer);
+                        
+            const file = new Blob([array.buffer]);
+
+            expect(array.length).toEqual(file.size);
+
             file.lastModifed = Date.now();
             file.lastModifedDate = new Date();
-            file.name = "test.xlsx";            
+            file.name = "test.xlsx";  
+                  
             input.files = [ file ];   
-            input.dispatchEvent(new CustomEvent('change'), { target: input } );   
-            return flushPromises();                     
+            input.dispatchEvent(new CustomEvent('change'), { target: input } );                
         })
+        .flushPromises()
+        .then( () => {
+            const modal = element.shadowRoot.querySelector('.slds-modal');
+            expect(modal).toBeTruthy();        
+ 
+            expect(modal.textContent).toMatch(/Reading File/);
+        })        
+        .flushPromisesUntil(5, () => {
+            const modal = element.shadowRoot.querySelector('.slds-modal');                
+            return modal.textContent.match(/Done/);
+        })
+        .then( () => {
+            expect(updateRecord).toHaveBeenCalled();
+            expect(updateRecord.mock.calls[0]).toEqual([
+                { 
+                    fields: {
+                        "Id": "00X123456789012345678",              
+                        "field2__c": "B2",
+                        "field3__c": 42,
+                        "field5__c": "AA10",
+                        "field8__c": "C1",
+                        "field9__c": "C2",
+                        "field10__c": "A1"                                  
+                    }
+                }
+            ]);
+
+        });    
     });
 });
